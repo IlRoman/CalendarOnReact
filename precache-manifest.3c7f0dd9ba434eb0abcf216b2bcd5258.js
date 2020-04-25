1f86a2e6141819b25ca8ce01f154a88af6f2d4b0
“Il_Roman” <il.roman@ukr.net>
2020-04-25 19:21:01 +0300

    Updates

---
+++ precache-manifest.3c7f0dd9ba434eb0abcf216b2bcd5258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707d0d2cee61c4939630b3a91ad7a45a",
+    "url": "/CalendarOnReact/index.html"
+  },
+  {
+    "revision": "650acd11f23d8683671b",
+    "url": "/CalendarOnReact/static/css/main.6369fbb2.chunk.css"
+  },
+  {
+    "revision": "e031f45543d72a6efdba",
+    "url": "/CalendarOnReact/static/js/2.e15dc63b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CalendarOnReact/static/js/2.e15dc63b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650acd11f23d8683671b",
+    "url": "/CalendarOnReact/static/js/main.28f4f78d.chunk.js"
+  },
+  {
+    "revision": "78e6db72070a00c6e5a7",
+    "url": "/CalendarOnReact/static/js/runtime-main.40f02486.js"
+  }
+]);
